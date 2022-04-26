0077ae6e1d302857f3df68e5c98daa1bb4d6071e
lanmower <almagestfraternite@gmail.com>
2022-04-26 23:13:28 +0200

    Update init.js

--- init.js
+++ init.js
@@ -3,9 +3,23 @@ const b32 = require('hi-base32');
 require('dotenv').config();
 const crypto = require("hypercore-crypto");
 const keyPair = crypto.keyPair(crypto.data(Buffer.from(process.env.KEY)));
-console.log('listening', b32.encode(keyPair.publicKey).replace('====','').toLowerCase()+".matic.ml");
-fs.mkdirSync('tunnel/greenlock.d/', { recursive: true }, (err) => {console.log(err)});
-fs.writeFileSync('tunnel/greenlock.d/config.json', JSON.stringify({sites:{subject:b32.encode(keyPair.publicKey).replace('====','').toLowerCase()+".matic.ml"}}));
-fs.mkdirSync('~/.config/code-server/', { recursive: true }, (err) => {console.log(err)});
-fs.writeFileSync('~/.config/code-server/config.yaml', 'bind-addr: 127.0.0.1:8080\nauth: password\npassword: '+keyPair.secretKey.toString('base64')+'\ncert: false');
-console.log("Password is " + keyPair.secretKey.toString('base64').replace('==',''));
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter a password: ', function (password) {
+  console.log('address will be: ', b32.encode(keyPair.publicKey).replace('====','').toLowerCase()+".matic.ml");
+  fs.mkdirSync('tunnel/greenlock.d/', { recursive: true }, (err) => {console.log(err)});
+  fs.writeFileSync('tunnel/greenlock.d/config.json', JSON.stringify({sites:{subject:b32.encode(keyPair.publicKey).replace('====','').toLowerCase()+".matic.ml"}}));
+  fs.mkdirSync('~/.config/code-server/', { recursive: true }, (err) => {console.log(err)});
+  fs.writeFileSync('~/.config/code-server/config.yaml', 'bind-addr: 127.0.0.1:8080\nauth: password\npassword: '+password+'\ncert: false');
+  rl.close();
+});
+
+rl.on('close', function () {
+  console.log('\nBYE BYE !!!');
+  process.exit(0);
+});
