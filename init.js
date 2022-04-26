7cbbad52d5ad89b51e1cc06da80d7a933b07c759
lanmower <almagestfraternite@gmail.com>
2022-04-27 01:50:32 +0200

    Update init.js

--- init.js
+++ init.js
@@ -9,16 +9,19 @@ const rl = readline.createInterface({
 });
 
 rl.question('Enter a unique identifier: ', function (password) {
-  const keyPair = crypto.keyPair(crypto.data(Buffer.from(password)));
-
-  console.log('Address will be: ', b32.encode(keyPair.publicKey).replace('====','').toLowerCase()+".matic.ml");
-  fs.mkdirSync('tunnel/greenlock.d/', { recursive: true }, (err) => {console.log(err)});
-  fs.writeFileSync('tunnel/.env', 'KEY='+password);
-  fs.writeFileSync('tunnel/greenlock.d/config.json', JSON.stringify({sites:[{subject:b32.encode(keyPair.publicKey).replace('====','').toLowerCase()+".matic.ml"}]}));
-  fs.mkdirSync('../.config/code-server/', { recursive: true }, (err) => {console.log(err)});
-  fs.writeFileSync('../.config/code-server/config.yaml', 'bind-addr: 127.0.0.1:8080\nauth: password\npassword: '+keyPair.secretKey.toString('base64').replace('==','')+'\ncert: false');
-  console.log('Password will be: '+keyPair.secretKey.toString('base64').replace('==',''));
-  rl.close();
+  rl.question('Enter your contact email: ', function (email) {
+    const keyPair = crypto.keyPair(crypto.data(Buffer.from(password)));
+    const bkey = b32.encode(keyPair.publicKey).replace('====','').toLowerCase();
+    console.log('Address will be: ', bkey+".matic.ml");
+    fs.mkdirSync('tunnel/greenlock.d/', { recursive: true }, (err) => {console.log(err)});
+    fs.writeFileSync('tunnel/.env', 'KEY='+password);
+    fs.writeFileSync('tunnel/greenlock.d/config.json', JSON.stringify({sites:[{subject:bkey+".matic.ml"}]}));
+    fs.mkdirSync('../.config/code-server/', { recursive: true }, (err) => {console.log(err)});
+    fs.writeFileSync('../.config/code-server/config.yaml', 'bind-addr: 127.0.0.1:8080\nauth: password\npassword: '+keyPair.secretKey.toString('base64').replace('==','')+'\ncert: false');
+    console.log('Password will be: '+keyPair.secretKey.toString('base64').replace('==',''));
+    fs.writeFileSync('address', bkey+".matic.ml");
+    rl.close();
+  }
 });
 
 rl.on('close', function () {
