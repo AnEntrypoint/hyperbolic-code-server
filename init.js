465f051295475a4ed669c15499b77408b5116454
lanmower <almagestfraternite@gmail.com>
2022-04-26 23:19:22 +0200

    Update init.js

--- init.js
+++ init.js
@@ -8,14 +8,15 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Enter a password: ', function (password) {
+rl.question('Enter a unique identifier: ', function (password) {
   const keyPair = crypto.keyPair(crypto.data(Buffer.from(password)));
 
   console.log('address will be: ', b32.encode(keyPair.publicKey).replace('====','').toLowerCase()+".matic.ml");
   fs.mkdirSync('tunnel/greenlock.d/', { recursive: true }, (err) => {console.log(err)});
   fs.writeFileSync('tunnel/greenlock.d/config.json', JSON.stringify({sites:{subject:b32.encode(keyPair.publicKey).replace('====','').toLowerCase()+".matic.ml"}}));
   fs.mkdirSync('~/.config/code-server/', { recursive: true }, (err) => {console.log(err)});
-  fs.writeFileSync('~/.config/code-server/config.yaml', 'bind-addr: 127.0.0.1:8080\nauth: password\npassword: '+password+'\ncert: false');
+  fs.writeFileSync('~/.config/code-server/config.yaml', 'bind-addr: 127.0.0.1:8080\nauth: password\npassword: '+keyPair.secretKey.toString('base64').replace('==','')+'\ncert: false');
+  console.log('password will be:'+keyPair.secretKey.toString('base64').replace('==',''));
   rl.close();
 });
 
