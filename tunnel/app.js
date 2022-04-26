9ae11380a4e21f34860045082c56d3c8ebea9a2b
lanmower <almagestfraternite@gmail.com>
2022-04-26 23:02:32 +0000

    port change

--- tunnel/app.js
+++ tunnel/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const exampleProxy = createProxyMiddleware({
-  target: 'http://localhost:3000',
+  target: 'http://localhost:8080',
   changeOrigin: true,
   ws: true, 
 });
