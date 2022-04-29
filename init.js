574f842acccf1bca031dcd88337e703aec037faa
lanmower <almagestfraternite@gmail.com>
2022-04-29 12:42:36 +0200

    Update init.js

--- init.js
+++ init.js
@@ -15,7 +15,7 @@ rl.question('Enter a unique identifier: ', function (password) {
     console.log('Address will be: ', bkey+".matic.ml");
     fs.mkdirSync('tunnel/greenlock.d/', { recursive: true }, (err) => {console.log(err)});
     fs.writeFileSync('tunnel/.env', 'KEY='+password);
-    fs.writeFileSync('tunnel/greenlock.d/config.json', JSON.stringify({sites:[{subject:bkey+".matic.ml"}, defaults:{subscriberEmail: email}]}));
+    fs.writeFileSync('tunnel/greenlock.d/config.json', JSON.stringify({sites: [{subject:bkey+".matic.ml"}],defaults: {subscriberEmail: email}}));
     fs.mkdirSync('../.config/code-server/', { recursive: true }, (err) => {console.log(err)});
     fs.writeFileSync('../.config/code-server/config.yaml', 'bind-addr: 127.0.0.1:8080\nauth: password\npassword: '+keyPair.secretKey.toString('base64').replace('==','')+'\ncert: false');
     console.log('Password will be: '+keyPair.secretKey.toString('base64').replace('==',''));
