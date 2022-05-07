917f904238f67107eb207c097ee5a13616df97cf
lanmower <almagestfraternite@gmail.com>
2022-05-07 09:22:12 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-deftimezone=Etc/GMT
 sleep 5 && sudo docker logs code-server & sudo docker run \
-  -d --name code-server --restart unless-stopped --network host -e "TZ=${$4:-$deftimezone}" \
+  -d --name code-server --restart unless-stopped --network host -e "TZ=$4" \
   -e "http=80" -e "https=443" -e "email=$1" -e "password=$2" -v "$HOME/coder:/home/coder/" \
   -u "$(id -u):$(id -g)" -e "DOCKER_USER=$USER" "almagest/hyperbolic-code-server" --password $3
