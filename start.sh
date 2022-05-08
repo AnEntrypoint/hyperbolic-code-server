1a06f9ac9b691ad13272c2e4e653e3ca7cb9d2b4
lanmower <almagestfraternite@gmail.com>
2022-05-08 09:37:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 sudo docker run \
   -d --name code-server --restart unless-stopped --network host -e "TZ=$4" \
   -e "email=$1" -e "password=$2" -v "$HOME/coder:/home/coder/" \
-  -u "$(id -u):$(id -g)" -e "DOCKER_USER=$USER" "almagest/hyperbolic-code-server$5" --password $3 ;
+  -u "$(id -u):$(id -g)" -e "DOCKER_USER=$USER" -e "PASSWORD=$3" "almagest/hyperbolic-code-server$5";
 sudo docker logs code-server -f
