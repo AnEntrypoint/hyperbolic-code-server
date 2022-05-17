465316500fd12f42526057d0454f4bc476b5b2d4
lanmower <almagestfraternite@gmail.com>
2022-05-17 10:10:39 +0200

    Update newhost.sh

--- newhost.sh
+++ newhost.sh
@@ -5,7 +5,7 @@ apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
