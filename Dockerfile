8fda1853634f7fb971bc32130e3710bdfd7d9d5c
av8ta <av8ta@protonmail.com>
2022-06-04 02:17:04 +0000

    rename start script to install. add ~/.profile to image point npm to ~/.global_node_modules for global installs

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,9 @@ RUN DEBIAN_FRONTEND="noninteractive" apt-get update -y && \
     apt-get install -y nodejs python3 build-essential tzdata libcap2-bin
 
 # now that node is installed; copy and install grain from dockerhub
-COPY --from=grainlang/grain:main /grain /grain
+COPY --from=grainlang/grain:main-slim /grain /grain
 WORKDIR /grain
-# Link CLI in new image
+# Link grain cli
 RUN npm run cli link
 
 RUN npm install -g pm2
@@ -17,15 +17,19 @@ RUN sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/node
 
 WORKDIR /home/coder
 
-# alias code so you can open files from terminal
-RUN if ! sudo cat /root/.bashrc | grep -q 'alias code="'; then sudo echo 'alias code="/usr/lib/code-server/bin/code-server"' >> /root/.bashrc; fi;
-
-# USER coder
+USER coder
 ENTRYPOINT sudo touch /home/coder/startup; \
+    # alias 'code' so you can open files from terminal \
+    touch /home/coder/.bashrc; if ! cat /home/coder/.bashrc | grep -q 'alias code="'; then echo 'alias code="/usr/lib/code-server/bin/code-server"' >> /home/coder/.bashrc; fi; \
+    # make an accessible global node_modules for userland installs \
+    if ! cat /home/coder/.bashrc | grep -q 'export PATH=/home/coder/.global_node_modules/bin:'; then echo "export PATH=/home/coder/.global_node_modules/bin:$PATH" >> /home/coder/.bashrc; fi; \
+    mkdir /home/coder/.global_node_modules; \
+    npm config set prefix "/home/coder/.global_node_modules"; \
+    # set permissions for grain and home \
     sudo chmod a+rw /grain -R; \
     sudo chmod a+rw /home/coder -R; \
     # decrease ssh private key permissions
-    if ls /home/coder/.ssh | grep "id_rsa"; then chmod 600 /home/coder/.ssh/id_rsa*; fi; \
+    if ls /home/coder/.ssh | grep "id_rsa"; then chmod 400 /home/coder/.ssh/id_rsa; fi; \
     # setup hyperbolic tunnel over hyperswarm \ 
     git clone https://github.com/AnEntrypoint/hyperbolic-tunnel /home/coder/hyperbolic-tunnel || true; \
     cd /home/coder/hyperbolic-tunnel; \
@@ -34,5 +38,4 @@ ENTRYPOINT sudo touch /home/coder/startup; \
     sleep 3 && \
     cat ~/.config/code-server/config.yaml & cd /home/coder; \
     if ! cat /etc/passwd | grep -q "coder:"; then echo "adding coder user"; echo "coder:x:1001:1001::/home/coder:/bin/bash" >> /etc/passwd; fi; \
-    source /root/.bashrc \
     cd /home/coder && sh /home/coder/startup & /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 .
--- start.sh
+++ start.sh
@@ -1,48 +1,23 @@
 #!/bin/bash
 
-# exit when any command fails
-set -e
-
-echo "your email for letsencrypt:"
-read email
-regex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
-checkemail() {
-        if [[ ! ($email =~ $regex) ]] ;
-                then echo "letsencrypt needs a valid email address"; exit 1;
-        fi
-}
-checkemail;
-
-echo "the hyper seed to generate a key from: (long and unique)"
-read seed
-
-echo "vscode login password:"
-read pw
-
-echo "timezone: "
-read tz
-if [[ $tz        == "" ]]; then tz=UTC ; echo "timezone is UTC"; fi
-
-echo "subdomain you want to advertise as: (<subdomain>.sites.247420.xyz)"
-read subdomain
-
-platform=""
-case $(uname -m) in
-    x86_64) platform="amd64" ;;
-    aarch64) platform="arm64" ;;
-    arm)    dpkg --print-architecture | grep -q "arm64" && platform="arm64" || platform="arm32" ;;
-esac
-echo $platform
-if [[ (! $platform  == "arm32" && ! $platform  == "arm64" && ! $platform == "amd64") ]]; then echo "arm or amd64 is needed to pull the right docker image to run"; exit 1; fi
-echo "platform is $platform. starting hyperbolic-grain now..."; echo "";
-
-if [[ $email     == "" ]]; then echo "email is needed for letsencrypt certificates"; exit 1; fi
-if [[ $seed      == "" ]]; then echo "hyper seed is needed to generate a unique hypercore-protocol address."; echo "anything long and unique will do."; echo "you don't need to save it for later :)"; exit 1; fi
-if [[ $pw        == "" ]]; then echo "password is needed for vscode as it will be accessible online"; exit 1; fi
-if [[ $subdomain == "" ]]; then echo "please set a subdomain to advertise on"; exit 1; fi
-
-docker run \
-  -d --name code-server --restart unless-stopped --network host -e "TZ=$tz" \
-  -e "email=$email" -e "password=$seed" -v "$HOME/coder:/home/coder/" \
-  -u "$(id -u):$(id -g)" -e "DOCKER_USER=$USER" -e "PASSWORD=$pw" -e "domainname=$subdomain" "av8ta/hyperbolic-grain:main-$platform";
-docker logs code-server -f
+if docker restart code-server; 
+  then 
+    hyperbolic_extern_ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}');
+    echo "enjoy hyperbolic-grain code-server on your local network at: http://$hyperbolic_extern_ip:8080";
+    echo "bind_addr: 0.0.0.0"
+    echo "on this device: http://localhost:8080 aka http://127.0.0.1:8080"; echo "";
+    echo "to update grain: pull, build, install:"; echo "";
+    echo "docker pull grainlang/grain:main-slim";
+    echo "./remove.sh";
+    echo "./build.sh";
+    echo "./install.sh"; echo "";
+    echo "you may also want to delete ~/coder/hyperbolic-tunnel which is bind mounted into the container.";
+    echo "deleting it will ensure the settings chosen when running install.sh are correctly applied.";
+    echo "do this before running ./install.sh";
+  else
+    echo ""
+    echo "you need to run install.sh to create the container.";
+    echo "if you have previously run install.sh ..."; echo "";
+    echo "you may also want to delete ~/coder/hyperbolic-tunnel which is bind mounted into the container.";
+    echo "deleting it will ensure the settings chosen when running install.sh are correctly applied.";
+fi
