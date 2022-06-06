915212d719a3401eecc5771f2ff3d9ee4a31dd2a
av8ta <av8ta@protonmail.com>
2022-06-06 14:49:59 +0800

    rename start script to install detect platform architecture for docker tags remove sudo from docker commands grep for user before adding to /etc/passwd run entrypoint script as coder user add "code" alias to .bashrc add sanity checks to user prompts in install.sh update url from matic.ml to sites.247420.xyz add stop script

--- start.sh
+++ start.sh
@@ -1,16 +1,18 @@
-echo "Your email for letsencrypt:"
-read email
-echo "The VSCODE login password:"
-read pw
-echo "The timezone (pick UTC if unsure):"
-read tz
-echo "The subdomain you want to advertise as:"
-read subdomain
-echo "arm or amd64:"
-read platform
+#!/bin/bash
 
-sudo docker run \
-  -d --name code-server --restart unless-stopped --network host -e "TZ=$tz" \
-  -e "email=$email" -e "password=$seed" -v "$HOME/coder:/home/coder/" \
-  -u "$(id -u):$(id -g)" -e "DOCKER_USER=$USER" -e "PASSWORD=$pw" -e "domainname=$subdomain" "almagest/hyperbolic-code-server-$platform";
-sudo docker logs code-server -f
+if docker restart code-server; 
+  then 
+    hyperbolic_extern_ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}');
+    echo "enjoy hyperbolic-code-server on your local network at: http://$hyperbolic_extern_ip:8080";
+    echo "bind_addr: 0.0.0.0"
+    echo "on this device: http://localhost:8080 aka http://127.0.0.1:8080"; echo "";
+    echo "you may also want to delete ~/coder/hyperbolic-tunnel which is bind mounted into the container.";
+    echo "deleting it will ensure the settings chosen when running install.sh are correctly applied.";
+    echo "do this before running ./install.sh";
+  else
+    echo ""
+    echo "you need to run install.sh to create the container.";
+    echo "if you have previously run install.sh ..."; echo "";
+    echo "you may also want to delete ~/coder/hyperbolic-tunnel which is bind mounted into the container.";
+    echo "deleting it will ensure the settings chosen when running install.sh are correctly applied.";
+fi
