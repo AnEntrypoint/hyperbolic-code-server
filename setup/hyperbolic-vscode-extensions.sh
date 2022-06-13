672614786f9e4bfe83a95c906a80535d8105ce2f
av8ta <av8ta@protonmail.com>
2022-06-13 23:25:46 +0800

    add max attempts waiting for extension host to start

--- setup/hyperbolic-vscode-extensions.sh
+++ setup/hyperbolic-vscode-extensions.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 # $1: filepath $2: url (often https://raw.githubusercontent.com/...)
-install_extension () {
-vscode_ext="$1"
-if [ -f "$vscode_ext" ];
-  then  echo "vscode extension $1 found, skipping download and install";
-  else  echo "vscode extension $1 not found, downloading from $2...";
-        curl --output /home/coder/"$1" "$2";
-        echo "installing extension: $1";
-        /usr/bin/code-server --install-extension /home/coder/"$1";
-fi;
+install_extension() {
+  vscode_ext="$1"
+  if [ -f "$vscode_ext" ]; then
+    echo "vscode extension $1 found, skipping download and install"
+  else
+    echo "vscode extension $1 not found, downloading from $2..."
+    curl --output /home/coder/"$1" "$2"
+    echo "installing extension: $1"
+    /usr/bin/code-server --install-extension /home/coder/"$1"
+  fi
 }
 
-sleep 10 &&
-while true
-do
-  sleep 5;
+sleep 1 && attempt=0
+max_attempts=50
+while true; do
+  if ((attempt == max_attempts)); then
+    echo "max attempts: $max_attempts waiting for extension host to start. aborting vscode extension installs."
+    echo "you can install them manually in vscode"
+    break
+  fi
+  sleep 5
   log_file=/home/coder/dockerlogs/install.log
-  if [[ ! -f "$log_file" ]] 2> /dev/null; then echo "no docker install log tailed into volume"; break; fi;
-  if grep -q -i --no-messages "Extension host agent started." /home/coder/dockerlogs/install.log;
-    then 
-      install_extension "vscode-grain.vsix" "https://raw.githubusercontent.com/av8ta/grain-language-server/master/editor-extensions/vscode/vscode-grain-0.16.0.vsix";
-      break;
-    else echo "waiting for extension host to start";
-  fi;
+  if [[ ! -f "$log_file" ]] 2>/dev/null; then
+    echo "no docker log tailed into volume"
+    break
+  fi
+  if grep -q -i --no-messages "Extension host agent started." /home/coder/dockerlogs/install.log; then
+    install_extension "vscode-grain.vsix" "https://raw.githubusercontent.com/av8ta/grain-language-server/master/editor-extensions/vscode/vscode-grain-0.16.0.vsix"
+    break
+  else
+    echo "attempt number $((attempt++)) waiting for extension host to start"
+  fi
 done
