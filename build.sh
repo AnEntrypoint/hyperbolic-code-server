5004c9486500b4e3a7e849be138afa8c77bf645b
lanmower <almagestfraternite@gmail.com>
2022-06-23 23:23:31 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-platform=""
-case $(uname -m) in
-    x86_64) platform="amd64" ;;
-    aarch64) platform="arm64" ;;
-    arm)    dpkg --print-architecture | grep -q "arm64" && platform="arm64" || platform="arm32" ;;
-esac
-if [[ (! $platform  == "arm32" && ! $platform  == "arm64" && ! $platform == "amd64") ]]; then echo "arm or amd64 is needed to pull the right docker image to run"; exit 1; fi
-
-docker_tag=almagest/hyperbolic-code-server-$platform
-echo "platform is $platform. docker tag will be: $docker_tag"; echo "";
-
-docker build -t "almagest/hyperbolic-code-server-$platform" .
+sudo docker build -t hyperbolic-code-server .
