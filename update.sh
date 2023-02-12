ca16a39fa26c9ce1eb89c1c35f824eb225f9e76c
lanmower <almagestfraternite@gmail.com>
2023-02-12 14:40:59 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -15,7 +15,7 @@ echo "arm or amd64:"
 read platform
 
 sudo docker run \
-  -d --name code-server --restart unless-stopped --network host -e "TZ=$tz" \
+  -d --name code-server --restart unless-stopped --publish-all -e "TZ=$tz" \
   -e "email=$email" -e "password=$seed" -v "$HOME/coder:/home/coder/" \
   -u "$(id -u):$(id -g)" -e "DOCKER_USER=$USER" -e "PASSWORD=$pw" -e "domainname=$subdomain" "almagest/hyperbolic-code-server-$platform";
 sudo docker logs code-server -f
