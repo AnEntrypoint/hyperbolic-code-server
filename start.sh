a62a8272bc398b0efa9cafde365a4a59452549e2
lanmower <almagestfraternite@gmail.com>
2023-06-02 09:46:57 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,7 +12,5 @@ read platform
 sudo docker run \
   -d --name code-server --restart unless-stopped -e "TZ=$tz" \
   -e "email=$email" -e "password=$seed" -v "$HOME/coder:/home/coder/" \
-  -u "$(id -u):$(id -g)" -e "DOCKER_USER=$USER" -e "PASSWORD=$pw" -e "domainname=$subdomain" \
-  -p 3000-4000:3000-4000 -p 80:80 -p 443:443 -p 8080:8080 \ 
-  "almagest/hyperbolic-code-server-$platform";
+  -u "$(id -u):$(id -g)" -e "DOCKER_USER=$USER" -e "PASSWORD=$pw" -e "domainname=$subdomain" -p 3000-4000:3000-4000 -p 80:80 -p 443:443 -p 8080:8080 "almagest/hyperbolic-code-server-$platform";
 sudo docker logs code-server -f
