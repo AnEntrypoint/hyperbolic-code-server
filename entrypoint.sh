30111b4ac9a23baea68c64e20465838395e2eac7
lanmower <almagestfraternite@gmail.com>
2023-08-10 14:55:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,19 @@
 
+sudo touch /home/coder/startup
+sudo chmod a+rw /home/coder -R
+git clone https://github.com/AnEntrypoint/hyperbolic-tunnel /home/coder/hyperbolic-tunnel
+cd /home/coder/hyperbolic-tunnel
+git pull
+npm install
+target=$target http=80 https=443 pm2 start runnode.js
+sleep 3
+cat ~/.config/code-server/config.yaml
+cd /home/coder
+if [ ! -f firstrundone ]
+    then echo first run
+    touch /home/coder/firstrundone
+    head -n -1 /etc/passwd > /tmp/passwd
+    sudo mv /tmp/passwd /etc/passwd
+fi
+cd /home/coder
+/home/coder/startup 1>startup.log 2>startup.err &
