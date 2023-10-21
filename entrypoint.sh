daa310554e6daff331b54d1154c9d10aae1b66d5
lanmower <almagestfraternite@gmail.com>
2023-10-21 20:40:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 sudo touch /home/coder/startup
 sudo chown coder:coder /home/coder -R
 git clone https://github.com/AnEntrypoint/hyperbolic-tunnel /home/coder/hyperbolic-tunnel
-rm /home/coder/entrypoint.sh
 cd /home/coder
-wget https://raw.githubusercontent.com/AnEntrypoint/hyperbolic-code-server/main/entrypoint.sh
 sudo chmod a+x /home/coder/entrypoint.sh
 sudo chmod a+x /home/coder/startup
 cd /home/coder/hyperbolic-tunnel
@@ -24,3 +22,5 @@ cd /home/coder
 
 /home/coder/startup 1>startup.log 2>startup.err &
 echo $PASSWORD
+wget https://raw.githubusercontent.com/AnEntrypoint/hyperbolic-code-server/main/entrypoint.sh
+rm /home/coder/entrypoint.sh
