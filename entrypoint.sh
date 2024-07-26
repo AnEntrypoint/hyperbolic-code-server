7e44d4a5ff1ac8b1b46fa08f6c366c4e9cc39507
lanmower <almagestfraternite@gmail.com>
2024-07-26 09:42:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,15 @@ git clone https://github.com/AnEntrypoint/hyperbolic-tunnel
 cd /root/hyperbolic-tunnel
 git pull
 npm install
-target=$target http=80 https=443 sudo pm2 start --name gate npx -- hyperbolic-tunnel
-cd /home/coder
-if [ ! -f firstrundone ]
+sudo pm2 start --name gate runnode.js
+cd /root
+if [ ! -f /root/firstrundone ]
     then echo first run
-    touch /home/coder/firstrundone
+    sudo touch /root/firstrundone
     head -n -1 /etc/passwd > /tmp/passwd
     sudo mv /tmp/passwd /etc/passwd
 fi
 cd /home/coder
-
 sh /home/coder/startup 1>startup.log 2>startup.err &
-echo $PASSWORD
-sudo wget https://raw.githubusercontent.com/AnEntrypoint/hyperbolic-code-server/main/entrypoint.sh -O entrypoint.sh
+sudo wget -r https://raw.githubusercontent.com/AnEntrypoint/hyperbolic-code-server/main/entrypoint.sh -O /root/entrypoint.sh
+sudo chmod a+x /root/entrypoint.sh
