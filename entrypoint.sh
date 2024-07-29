001175b19a81fa9c9cad096b478dead8cb0ce844
lanmower <almagestfraternite@gmail.com>
2024-07-29 17:43:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,26 @@
-cd /root
-git clone https://github.com/AnEntrypoint/hyperbolic-tunnel
+#!/bin/sh
+
+# Clone and prepare the hyperbolic-tunnel repository
+git clone https://github.com/AnEntrypoint/hyperbolic-tunnel /root/hyperbolic-tunnel || (cd /root/hyperbolic-tunnel && git pull)
 cd /root/hyperbolic-tunnel
-git pull
-npm install
-cd /root
-if [ ! -f /root/firstrundone ]
-    then echo first run
-    sudo touch /root/firstrundone
-    head -n -1 /etc/passwd > /tmp/passwd
-    sudo mv /tmp/passwd /etc/passwd
+npm install --unsafe-perm
+
+# Create a marker for the first run
+if [ ! -f /root/firstrundone ]; then
+    echo "first run"
+    touch /root/firstrundone
+    cp /etc/passwd /tmp/passwd
+    chown 1001:1001 /tmp/passwd
+    mv /tmp/passwd /etc/passwd
 fi
-touch /home/coder/startup
+
+# Start the application with PM2
 pm2 start --name gate runnode.js
-cd /home/coder
+
+# Create a startup script
+touch /home/coder/startup
 sh /home/coder/startup 1>startup.log 2>startup.err &
-wget -r https://raw.githubusercontent.com/AnEntrypoint/hyperbolic-code-server/main/entrypoint.sh -O /root/entrypoint.sh
+
+# Download the new entrypoint script
+wget -q -O /root/entrypoint.sh https://raw.githubusercontent.com/AnEntrypoint/hyperbolic-code-server/main/entrypoint.sh
 chmod a+x /root/entrypoint.sh
