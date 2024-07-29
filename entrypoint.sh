625bffcc2f126f775bc77d4f0fbb4e6d16e89a96
lanmower <almagestfraternite@gmail.com>
2024-07-29 17:32:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
-sudo touch /home/coder/startup
 cd /root
 git clone https://github.com/AnEntrypoint/hyperbolic-tunnel
 cd /root/hyperbolic-tunnel
 git pull
 npm install
-sudo pm2 start --name gate runnode.js
 cd /root
 if [ ! -f /root/firstrundone ]
     then echo first run
@@ -12,6 +10,8 @@ if [ ! -f /root/firstrundone ]
     head -n -1 /etc/passwd > /tmp/passwd
     sudo mv /tmp/passwd /etc/passwd
 fi
+sudo touch /home/coder/startup
+sudo pm2 start --name gate runnode.js
 cd /home/coder
 sh /home/coder/startup 1>startup.log 2>startup.err &
 sudo wget -r https://raw.githubusercontent.com/AnEntrypoint/hyperbolic-code-server/main/entrypoint.sh -O /root/entrypoint.sh
