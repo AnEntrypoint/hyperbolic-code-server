0132e2909f2219a332f9e64fa65d5a0893322a76
lanmower <almagestfraternite@gmail.com>
2024-07-29 17:52:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,44 @@
 #!/bin/sh
 
+# Function to ensure commands have the right permissions
+set_permissions() {
+    local dir="$1"
+    local user="$2"
+    local group="$3"
+    
+    mkdir -p "$dir"
+    chown -R "$user:$group" "$dir"
+}
+
 # Clone and prepare the hyperbolic-tunnel repository
-if [ ! -d "/root/hyperbolic-tunnel" ]; then
-    git clone https://github.com/AnEntrypoint/hyperbolic-tunnel /root/hyperbolic-tunnel
+if [ ! -d "/home/coder/hyperbolic-tunnel" ]; then
+    git clone https://github.com/AnEntrypoint/hyperbolic-tunnel /home/coder/hyperbolic-tunnel
 else
-    (cd /root/hyperbolic-tunnel && git pull)
+    (cd /home/coder/hyperbolic-tunnel && git pull)
 fi
 
-cd /root/hyperbolic-tunnel
+cd /home/coder/hyperbolic-tunnel || exit
 npm install --unsafe-perm
 
-# Create a marker for the first run and fix ownership issues
-if [ ! -f /root/firstrundone ]; then
+# Create a marker for the first run
+if [ ! -f /home/coder/firstrundone ]; then
     echo "first run"
-    touch /root/firstrundone
+    touch /home/coder/firstrundone
     cp /etc/passwd /tmp/passwd
     echo "coder:x:1001:1001::/home/coder:/bin/bash" >> /tmp/passwd
     mv /tmp/passwd /etc/passwd
-    # Ensure that the coder user can access npm's cache directory
-    mkdir -p /home/coder/.npm
-    chown -R coder:coder /home/coder/.npm
 fi
 
+# Fix permissions for npm and PM2 directories
+set_permissions "/home/coder/.npm" "coder" "coder"
+set_permissions "/home/coder/.pm2" "coder" "coder"
+
 # Start the application with PM2
-pm2 start --name gate runnode.js
+sudo -u coder pm2 start --name gate runnode.js
 
 # Create a startup script
-touch /home/coder/startup
-sh /home/coder/startup 1>startup.log 2>startup.err &
+sudo -u coder touch /home/coder/startup
+sudo -u coder sh /home/coder/startup 1>startup.log 2>startup.err &
 
 # Download the new entrypoint script
 wget -q -O /home/coder/entrypoint.sh https://raw.githubusercontent.com/AnEntrypoint/hyperbolic-code-server/main/entrypoint.sh
